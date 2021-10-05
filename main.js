5155306b11daeb69c0d78e1bebd0d48a80d4355d
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2021-10-04 22:13:29 -0300

    Auto stash before merge of "L3-end" and "origin/L3-end"

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/'
-            // solution
+            url: 'https://www.google.com/search?q=vue+socks&sxsrf=AOaemvKlllpUOOgtNw3_ma21rOyutyJymg:1633395663290&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi_mKaCibLzAhXNrJUCHYgYBMkQ_AUoAnoECAEQBA&biw=1093&bih=969&dpr=1'
         }
     }
 })
